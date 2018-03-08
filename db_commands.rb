2c4ebdf590e035a0aadcbd7ba66acd57a32f550a
Patrick <patrick.chao.14@gmail.com>
2018-03-08 15:45:24 -0800

    Modify db commands that return a single row to de-nest the result array

--- db_commands.rb
+++ db_commands.rb
@@ -13,11 +13,11 @@ def insert_user(con, email_address)
 end
 
 def find_user_by_id(id)
-  con.exec "SELECT * FROM users WHERE id = #{id}"
+  (con.exec "SELECT * FROM users WHERE id = #{id}")[0]
 end
 
 def find_user_by_email(email)
-  con.exec "SELECT * FROM users WHERE email_address = #{email}"
+  (con.exec "SELECT * FROM users WHERE email_address = #{email}")[0]
 end
 
 def insert_license_key(con, user_id, license_key)
@@ -27,11 +27,11 @@ def insert_license_key(con, user_id, license_key)
 end
 
 def find_license_key_by_user_id(user_id)
-  con.exec "SELECT * FROM license_keys WHERE user_id = #{user_id}"
+  (con.exec "SELECT * FROM license_keys WHERE user_id = #{user_id}")[0]
 end
 
 def find_license_key_by_id(id)
-  con.exec "SELECT * FROM license_keys WHERE id = #{id}"
+  (con.exec "SELECT * FROM license_keys WHERE id = #{id}")[0]
 end
 
 def insert_order(con, customer_id, shop_id)
@@ -41,13 +41,13 @@ def insert_order(con, customer_id, shop_id)
 end
 
 def find_order_by_id(id)
-  con.exec "SELECT * FROM orders WHERE id = #{id}"
+  (con.exec "SELECT * FROM orders WHERE id = #{id}")[0]
 
 end
 def find_order_by_customer_id(customer_id)
-  con.exec "SELECT * FROM orders WHERE customer_id = #{customer_id}"
+  (con.exec "SELECT * FROM orders WHERE customer_id = #{customer_id}")[0]
 
 end
 def find_order_by_shop_id(shop_id)
-  con.exec "SELECT * FROM orders WHERE shop_id = #{shop_id}"
+  (con.exec "SELECT * FROM orders WHERE shop_id = #{shop_id}")[0]
 end
