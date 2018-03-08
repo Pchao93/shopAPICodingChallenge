e591637bbf5cc2bf52c991063d3eb38a3d1b20cf
Patrick <patrick.chao.14@gmail.com>
2018-03-08 15:21:40 -0800

    Update null validations

--- seed.rb
+++ seed.rb
@@ -13,20 +13,20 @@ ensure
   postgres.close if postgres
 end
 
-#connect to our database
+# connect to our database
 con = PG.connect(dbname: "shopapi")
 
 
 p con.server_version
 
-#drop any pre-existing tables
+# drop any pre-existing tables
 con.exec "DROP TABLE IF EXISTS orders"
 con.exec "DROP TABLE IF EXISTS license_keys"
 con.exec "DROP TABLE IF EXISTS users"
 
 # create user table
-con.exec "CREATE TABLE users(id INTEGER PRIMARY KEY,
-        email_address VARCHAR(255), num_license_keys_sent INTEGER)"
+con.exec "CREATE TABLE users(id INTEGER PRIMARY KEY NOT NULL,
+        email_address VARCHAR(255) NOT NULL, num_license_keys_sent INTEGER NOT NULL DEFAULT 0)"
 
 # add indices and uniqueness constraints
 con.exec "CREATE UNIQUE INDEX index_users_on_id ON users (id);"
@@ -41,8 +41,8 @@ con.exec "INSERT INTO users VALUES(5,'cool_shop@test.com',0)"
 con.exec "INSERT INTO users VALUES(6,'bad_shop@test.com',0)"
 
 # create license_key table
-con.exec "CREATE TABLE license_keys(id INTEGER PRIMARY KEY, user_id INTEGER REFERENCES users(id),
-        license_key VARCHAR(255))"
+con.exec "CREATE TABLE license_keys(id INTEGER PRIMARY KEY NOT NULL, user_id INTEGER NOT NULL REFERENCES users(id),
+        license_key VARCHAR(255) NOT NULL)"
 
 # add indices and uniqueness constraints
 con.exec "CREATE UNIQUE INDEX index_license_keys_on_id ON license_keys (id);"
@@ -55,8 +55,8 @@ con.exec "INSERT INTO license_keys VALUES(2,3,'cat')"
 con.exec "INSERT INTO license_keys VALUES(3,4,'dog')"
 
 # create orders table
-con.exec "CREATE TABLE orders(id INTEGER PRIMARY KEY, customer_id INTEGER REFERENCES users(id),
-        shop_id INTEGER REFERENCES users(id))"
+con.exec "CREATE TABLE orders(id INTEGER PRIMARY KEY NOT NULL, customer_id INTEGER NOT NULL REFERENCES users(id),
+        shop_id INTEGER NOT NULL REFERENCES users(id))"
 
 # add indices and uniqueness constraints
 con.exec "CREATE UNIQUE INDEX index_orders_on_id ON orders (id);"
