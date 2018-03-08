61b0b91c72e13456388dffcecab9c5501a1dda8b
Patrick <patrick.chao.14@gmail.com>
2018-03-08 14:24:32 -0800

    Basic rack server

--- main.rb
+++ main.rb
@@ -1,3 +1,20 @@
 require 'pg'
+require 'rack'
 
 postgres = PG.connect(dbname: 'postgres')
+postgres.exec("CREATE DATABASE shopAPI")
+
+
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = "text/text"
+  res.write(req.path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
