6f35b71cfa8802c57b26f84d68032bdf93ee99b6
Andrew <andrewdblum@gmail.com>
2018-03-08 16:19:06 -0800

    json parsing for post request

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'pg'
 require 'rack'
-require 'rack/lobster'
+require 'json'
+# require 'rack/lobster'
 
 postgres = PG.connect(dbname: 'postgres')
 begin
@@ -13,46 +14,58 @@ conn = PG.connect(dbname: "shopapi")
 
 p conn
 
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  flag = 'Success'
+  blob = JSON.parse req.body.read
+  userID = blob["userID"]
+  userID_customer = blob["userID_customer"]
+  licenseKey = blob["licenseKey"]
+  orderID = blob["orderID"]
 
 
-# app = Proc.new do |env|
-#   req = Rack::Request.new(env)
-#   res = Rack::Response.new
-#   res['Content-Type'] = "text/text"
-#   res.write(req.path)
-#   res.finish
-# end
+## db stuff goes here
 
-app = Rack::Lobster.new
+
+  flag = 'Failure' if (false)
+  res['Content-Type'] = "text/text"
+  res.write(flag)
+  res.finish
+end
+
+# app = Rack::Lobster.new
 
 Rack::Server.start(
   app: app,
   Port: 4000
 )
 
-while session = server.accept
-  request = session.gets
-  puts request
-
-  # 1
-  method, full_path = request.split(' ')
-  # 2
-  path, query = full_path.split('?')
-
-  # 3
-  status, headers, body = app.call({
-    'REQUEST_METHOD' => method,
-    'PATH_INFO' => path,
-    'QUERY_STRING' => query
-  })
-
-  session.print "HTTP/1.1 #{status}\r\n"
-  headers.each do |key, value|
-    session.print "#{key}: #{value}\r\n"
-  end
-  session.print "\r\n"
-  body.each do |part|
-    session.print part
-  end
-  session.close
-end
+# while session = server.accept
+#   request = session.gets
+#   pos = session.posts
+#   puts pos
+#   puts request
+#
+#   # 1
+#   method, full_path = request.split(' ')
+#   # 2
+#   path, query = full_path.split('?')
+#
+#   # 3
+#   status, headers, body = app.call({
+#     'REQUEST_METHOD' => method,
+#     'PATH_INFO' => path,
+#     'QUERY_STRING' => query
+#   })
+#
+#   session.print "HTTP/1.1 #{status}\r\n"
+#   headers.each do |key, value|
+#     session.print "#{key}: #{value}\r\n"
+#   end
+#   session.print "\r\n"
+#   body.each do |part|
+#     session.print part
+#   end
+#   session.close
+# end
